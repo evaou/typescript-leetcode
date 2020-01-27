f540791edfe3585dfe2f4645bc454fe5e4a1ad63
evaou <speeder528@gmail.com>
2020-01-27 17:18:27 +0800

    solve problem 242

---
+++ js/242.valid-anagram.js
@@ -0,0 +1,83 @@
+"use strict";
+/*
+ * @lc app=leetcode id=242 lang=javascript
+ *
+ * [242] Valid Anagram
+ *
+ * https://leetcode.com/problems/valid-anagram/description/
+ *
+ * algorithms
+ * Easy (54.81%)
+ * Likes:    1084
+ * Dislikes: 128
+ * Total Accepted:    461.6K
+ * Total Submissions: 841.9K
+ * Testcase Example:  '"anagram"\n"nagaram"'
+ *
+ * Given two strings s and t , write a function to determine if t is an anagram
+ * of s.
+ *
+ * Example 1:
+ *
+ *
+ * Input: s = "anagram", t = "nagaram"
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: s = "rat", t = "car"
+ * Output: false
+ *
+ *
+ * Note:
+ * You may assume the string contains only lowercase alphabets.
+ *
+ * Follow up:
+ * What if the inputs contain unicode characters? How would you adapt your
+ * solution to such case?
+ *
+ */
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+    var sObj = {};
+    for (var _i = 0, s_1 = s; _i < s_1.length; _i++) {
+        var char = s_1[_i];
+        if (char in sObj) {
+            sObj[char] += 1;
+        }
+        else {
+            sObj[char] = 1;
+        }
+    }
+    var tObj = {};
+    for (var _a = 0, t_1 = t; _a < t_1.length; _a++) {
+        var char = t_1[_a];
+        if (char in tObj) {
+            tObj[char] += 1;
+        }
+        else {
+            tObj[char] = 1;
+        }
+    }
+    for (var char in sObj) {
+        if (!(char in tObj)) {
+            return false;
+        }
+        if (sObj[char] !== tObj[char]) {
+            return false;
+        }
+    }
+    return true;
+};
+// @lc code=end
+//# sourceMappingURL=242.valid-anagram.js.map
---
+++ ts/242.valid-anagram.ts
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode id=242 lang=javascript
+ *
+ * [242] Valid Anagram
+ *
+ * https://leetcode.com/problems/valid-anagram/description/
+ *
+ * algorithms
+ * Easy (54.81%)
+ * Likes:    1084
+ * Dislikes: 128
+ * Total Accepted:    461.6K
+ * Total Submissions: 841.9K
+ * Testcase Example:  '"anagram"\n"nagaram"'
+ *
+ * Given two strings s and t , write a function to determine if t is an anagram
+ * of s.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "anagram", t = "nagaram"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "rat", t = "car"
+ * Output: false
+ * 
+ * 
+ * Note:
+ * You may assume the string contains only lowercase alphabets.
+ * 
+ * Follow up:
+ * What if the inputs contain unicode characters? How would you adapt your
+ * solution to such case?
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s: string, t: string) {
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    interface CountObj {
+        [key: string]: number;
+    }
+
+    let sObj: CountObj = {};
+    for (let char of s) {
+        if (char in sObj) {
+            sObj[char] += 1; 
+        } else {
+            sObj[char] = 1;
+        }
+    }
+
+    let tObj: CountObj = {};
+    for (let char of t) {
+        if (char in tObj) {
+            tObj[char] += 1;
+        } else {
+            tObj[char] = 1;
+        }
+    }
+
+    for (let char in sObj) {
+        if (!(char in tObj)) {
+            return false;
+        } 
+        if (sObj[char] !== tObj[char]) {
+            return false;
+        }
+    }
+
+    return true;
+};
+// @lc code=end
+
