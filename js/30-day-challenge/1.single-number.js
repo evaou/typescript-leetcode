aaf596f7e5676b0dd6a5b7c51a381b9c21004fa6
evaou <speeder528@gmail.com>
2020-04-01 20:59:49 +0800

    solve single number

---
+++ js/30-day-challenge/1.single-number.js
@@ -0,0 +1,25 @@
+"use strict";
+// Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+// Note:
+// Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+// Example 1:
+// Input: [2,2,1]
+// Output: 1
+// Example 2:
+// Input: [4,1,2,1,2]
+// Output: 4
+function singleNumber(nums) {
+    let frq = {};
+    for (let num of nums) {
+        if (num in frq) {
+            delete frq[num];
+        }
+        else {
+            frq[num] = 1;
+        }
+    }
+    return +Object.keys(frq)[0];
+}
+console.log(singleNumber([2, 2, 1]));
+console.log(singleNumber([4, 1, 2, 1, 2]));
+//# sourceMappingURL=1.single-number.js.map
