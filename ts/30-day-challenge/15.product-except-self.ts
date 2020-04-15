1e0f2dce91a869224936d9d2971f3536e171db23
evaou <speeder528@gmail.com>
2020-04-15 22:26:50 +0800

    solve product except self

---
+++ ts/30-day-challenge/15.product-except-self.ts
@@ -0,0 +1,32 @@
+/*
+Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Example:
+
+Input:  [1,2,3,4]
+Output: [24,12,8,6]
+Constraint: It's guaranteed that the product of the elements of any prefix or suffix of the array (including the whole array) fits in a 32 bit integer.
+
+Note: Please solve it without division and in O(n).
+*/
+
+function productExceptSelf(nums: number[]): number[] {
+  let forwardNums: number[] = Array(nums.length).fill(1);
+  let backwardNums: number[] = Array(nums.length).fill(1);
+  let resultNums: number[] = [];
+  let j: number;
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    j = nums.length - 1 - i;
+    forwardNums[i + 1] = forwardNums[i] * nums[i];
+    backwardNums[j - 1] = backwardNums[j] * nums[j];
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    resultNums.push(forwardNums[i] * backwardNums[i]);
+  }
+
+  return resultNums;
+}
+
+console.log(productExceptSelf([1, 2, 3, 4])); // [24, 12, 8, 6]
